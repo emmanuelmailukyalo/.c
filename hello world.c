96fca052ceba534b25a123a11864b19eb4faa97d
Emmanuel Mailu <emmanuelmailu.kyalo@gmail.com>
2022-03-14 10:06:35 +0300

    first commit

---
+++ hello world.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("hello, world!");
+}
+
+// variables
+int age =37;
+
+#include <stdio.h>
+
+int main(void);
+    int age = 0;
+    age = 11;
+    printf("%u" , age );
+
+#include <stdio.h>
+
+int main (void)
+{
+    unsigned char j = 255;
+    j = j + 10;
+    printf("%u", j);
+}
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world")
+    return 0;
+}
---
+++ myfirstprogramm.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("i am learning C.")
+  return 0;
+}
+
+/*add /n to breakline
+to add a blank line use two \n
+*/
+
+//for short comments
+
+//variables
+
+int myNUM = 15;
+
+/*Note: If you assign a new value to
+an existing variable
+it will overwrite the previous value:eg*/
+
+int myNUM = 15;  //my num is 15
+int myNUM = 26:  // now myNum is 26
+
+printf("myNUM");  //error
+// use format specifiers
+// %d or %i .. eg
+
+int myNUM = 30;  //now my var is 30
+printf("%d", myNUM;)  //outputs 15
+
+//let me try something by myself
+
+#include <stdio.h>
+
+int(main)
+{
+    //creating a variable
+    int JOSH = 30;
+    printf("%d", JOSH);   //prints 30
+}
+
+/*%d and %i(format specifier) is for int we
+*use %f for float
+*%c is for char*/
+
+/*  int= integer(whole number)
+char= character(letter)
+float= floating point number (decimal points)*/
+
+//let me try something by myself to test float and char
+
+char mailu = mailu;
+printf("%c", mailu); // mailu
+
+float dec1 = 45.90;
+printf("%f", dec1);  //45.90
+
+//learn't about format specifiers
+
+//will learn about variable operations eg adding and multiplication

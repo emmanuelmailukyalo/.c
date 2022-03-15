275f7754734ca483e4f2e160ff0cffd080a69cd9
Emmanuel Mailu <emmanuelmailu.kyalo@gmail.com>
2022-03-15 09:01:36 +0300

    created loop.c file

---
+++ loops.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main ()
+{
+
+int i = 0 , j ;
+
+{
+while (i<5)
+
+   j=0;
+   while (j<3);
+
+      {
+        printf("2");
+      }
+    
+{
+    printf("\n");
+    i++;
+}
+    
+}
+
+return 0;
+}

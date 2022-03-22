be0dd681fc1800bb0d2be84912a58e6a85038303
Emmanuel Mailu <emmanuelmailu.kyalo@gmail.com>
2022-03-22 10:32:36 +0300

    this is a staged change

--- c/kojm.c
+++ c/kojm.c
@@ -4,5 +4,7 @@ int x = 5;
 int y = 6;
 
 int sum = x + y;
+
 printf("%d", sum);
+
 return 0;
---
+++ c/prog.c
@@ -0,0 +1,13 @@
+//don't know whats wrong with my software programming journey
+// iim not understanding anything
+//i can't compile and run anything
+//where there is no willingness there must b excuses
+// i am not willing to lesarn c
+//hasty shortcats will lead to poverty
+//i like shortcuts
+//when u find something to do do it with all ur might 
+//i lack motivation
+//and grit
+
+//grit is understanding that u can improve not by ur intelligence but by learning
+/*soln create a learning formula and find motivation for this prog*\
